ef4973f23ce789407b114f1ce4489456a6f141ee
Elitemineking <65436860+hridyan@users.noreply.github.com>
2023-04-24 20:10:39 +0530

    Setup the randomizer for the computer

--- script.js
+++ script.js
@@ -2,9 +2,26 @@
 Start function
 -On start ask for `to what score`.
 -Set user input to max possible score
-play function
 -select a random choice via computer
 -compare the choices in regular rock paper scissors manner
 score function
 -if computer wins add 1 to computer score and vice versa.
 -if either score equals to inital user input end game and the scorer wins */
+let finalScore;
+function start(){
+    finalScore = prompt(`To what score do u want 2 play till`, `Enter winning points here`)
+}
+let compPlay=`none`
+function computerChoice(){
+    let compChoice = Math.floor((Math.random()*3)+1);
+    if (compChoice === 1){
+        compPlay = `rock`
+    } else if (compChoice === 2){
+        compPlay = `paper`
+    } else if (compChoice === 3){
+        compPlay = `scissors`
+    }
+    console.log(compPlay)
+    }
+
+
