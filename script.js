2009cfba9400c46f73bb3f7cd0f33d7dd249dc97
Elitemineking <65436860+hridyan@users.noreply.github.com>
2023-04-24 19:42:59 +0530

    Setu step-by-step for coding procedure.

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Game is in 3 parts: - 
+Start function
+-On start ask for `to what score`.
+-Set user input to max possible score
+play function
+-select a random choice via computer
+-compare the choices in regular rock paper scissors manner
+score function
+-if computer wins add 1 to computer score and vice versa.
+-if either score equals to inital user input end game and the scorer wins */
