19d433bdfde8e953e65f503aaa4fbe31669ccb05
Elitemineking <65436860+hridyan@users.noreply.github.com>
2023-04-27 19:38:08 +0530

    Finish Project
    Adds win conditions and selection checks and comparisions.
    Also clean code

--- script.js
+++ script.js
@@ -8,8 +8,24 @@ score function
 -if computer wins add 1 to computer score and vice versa.
 -if either score equals to inital user input end game and the scorer wins */
 let finalScore;
+let scorePlayer;
+let scoreRobot;
+
 function start(){
+    scorePlayer = 0;
+    scoreRobot = 0;
     finalScore = prompt(`To what score do u want 2 play till`, `Enter winning points here`)
+    console.clear()
+    while (scorePlayer != finalScore|| scoreRobot != finalScore ){
+        play();
+        if (scorePlayer == finalScore){
+            alert(`Player Wins`)
+            return
+        } else if (scoreRobot == finalScore) {
+            alert(`Robot wins`)
+            return
+        }
+    }
 }
 let compPlay=`none`
 function computerChoice(){
@@ -21,7 +37,62 @@ function computerChoice(){
     } else if (compChoice === 3){
         compPlay = `scissors`
     }
-    console.log(compPlay)
     }
 
+function play(){
+    let yourMove = prompt(`Enter rock, paper or scissors below`,`Enter here`);
+    yourMove = yourMove.toString().toLowerCase();
+    if (yourMove === 'rock'|| yourMove === 'paper' || yourMove === 'scissors'){
+        computerChoice();
+        console.log(`Player vs Computer`)
+        /*  7 cases
+        Computer    Player 
+        Comp-move == player-move
+        rock vs paper - comp-wins
+        paper vs scissor - comp wins
+        scissors vs rock - comp wins 
+        and opposites */
 
+        if (compPlay === yourMove){
+            console.log(`Player played ${yourMove} and computer played ${compPlay}`)
+            console.log(`Player score- ${scorePlayer} and Computer score - ${scoreRobot}`)
+            console.log(`Tie`)
+        } else if (compPlay === `rock` && yourMove ===`paper`){
+            scorePlayer+=1;
+            console.log(`Player won dis round`)
+            console.log(`Player played ${yourMove} and computer played ${compPlay}`)
+            console.log(`Player score- ${scorePlayer} and Computer score - ${scoreRobot}`)
+        }else if (compPlay === `paper` && yourMove ===`scissors`){
+            scorePlayer+=1;
+            console.log(`Player won dis round`)
+            console.log(`Player played ${yourMove} and computer played ${compPlay}`)
+            console.log(`Player score- ${scorePlayer} and Computer score - ${scoreRobot}`)
+        }else if (compPlay === `scissors` && yourMove ===`rock`){
+            scorePlayer+=1;
+            console.log(`Player won dis round`)
+            console.log(`Player played ${yourMove} and computer played ${compPlay}`)
+            console.log(`Player score- ${scorePlayer} and Computer score - ${scoreRobot}`)
+        }
+        else if (compPlay === `paper` && yourMove ===`rock`){
+            scoreRobot+=1;
+            console.log(`Computer won dis round`)
+            console.log(`Player played ${yourMove} and computer played ${compPlay}`)
+            console.log(`Player score- ${scorePlayer} and Computer score - ${scoreRobot}`)
+        }else if (compPlay === `scissors` && yourMove ===`paper`){
+            scoreRobot+=1;
+            console.log(`Computer won dis round`)
+            console.log(`Player played ${yourMove} and computer played ${compPlay}`)
+            console.log(`Player score- ${scorePlayer} and Computer score - ${scoreRobot}`)
+        }else if (compPlay === `rock` && yourMove ===`scissors`){
+            scoreRobot+=1;
+            console.log(`Computer won dis round`)
+            console.log(`Player played ${yourMove} and computer played ${compPlay}`)
+            console.log(`Player score- ${scorePlayer} and Computer score - ${scoreRobot}`)
+        }
+
+        
+    } else {
+        console.log(`Not a valid move`)
+        play();
+    }
+}
